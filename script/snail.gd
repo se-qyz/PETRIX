204d3960632a58bd3366c0b69301bdddae64efa6
Sean <dman63410@gmail.com>
2026-09-17 14:54:06 +0800

    files

---
+++ script/snail.gd
@@ -0,0 +1,19 @@
+extends Area2D
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+const SPEED = 100.0
+var direction = -1.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	position.x += direction * SPEED * delta
+
+
+func _on_timer_timeout() -> void:
+	direction *= -1
+	animated_sprite_2d.flip_h = !animated_sprite_2d.flip_h
